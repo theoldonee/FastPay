c10fb94dfeb6cc96967113d08da5b39fb4a6da37
theoldonee <rouvaf123@gmail.com>
2026-06-06 12:44:21 +0400

    feat (contracts/employee): add CreateEmployeeRequest contract

---
+++ FastPay.Server/Contracts/Employee/CreateEmployeeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastPay.Server.Contracts.Employee
+{
+    public class CreateEmployeeRequest
+    {
+        [Required]
+        public string firstName;
+
+        [Required]
+        public string lastName;
+
+        [Range (0.01, double.MaxValue )]
+        public decimal hourlyRate;
+    }
+}
