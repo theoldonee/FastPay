b721af6dd5bb1de171fe5cea2350c63aa8a0ce17
theoldonee <rouvaf123@gmail.com>
2026-06-06 22:51:00 +0400

    feat(controller/EmployeeController): add E mployeeController class

---
+++ FastPay.Server/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using FastPay.Server.Contracts.Employee;
+using FastPay.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastPay.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(CreateEmployeeRequest request)
+        {
+
+            Employee employee = new Employee
+            {
+                FirstName = request.firstName,
+                LastName = request.lastName,
+                CreatedAt = DateTime.Now,
+                HourlyRate = request.hourlyRate
+            };
+
+            //Service writes to db
+
+            
+            return Ok() ;
+            
+        }
+
+    }
+}
