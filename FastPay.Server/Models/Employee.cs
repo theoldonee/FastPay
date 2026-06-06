0aa3e8be718a194fdad8d630ff358f129c2e65d5
theoldonee <rouvaf123@gmail.com>
2026-06-06 13:02:08 +0400

    refactor(models/employee): remove excess code

--- FastPay.Server/Models/Employee.cs
+++ FastPay.Server/Models/Employee.cs
@@ -4,21 +4,13 @@ namespace FastPay.Server.Models
 {
     public class Employee : IEmployee
     {
-        public string FirstName { get => _firstName; set => _firstName = value; }
-        private string _firstName = string.Empty; 
+        public string FirstName { get; set; }
 
-        public string LastName { get => _lastName; set => _lastName = value; }
-        private string _lastName = string.Empty;
-
-        public decimal HourlyRate { get => _hourleyRate; set => _hourleyRate = value; }
-        private decimal _hourleyRate;
-        public Guid Id { get => _id; set => _id = value; }
-        private Guid _id;
-        public bool IsActive { get => _isActive; set => _isActive =  value; }
-        private bool _isActive;
-        public DateTime CreatedAt { get => _createdAt; set => _createdAt = value; }
-        private DateTime _createdAt;
-        public DateTime UpdatedAt { get => _updatedAt; set => _updatedAt = value; }
-        private DateTime _updatedAt;
+        public string LastName { get; set; }
+        public decimal HourlyRate { get; set; }
+        public Guid Id { get; set ; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set ; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
