13e02d1d9be1a0007d9fc214c5cd7ad595a82f06
theoldonee <rouvaf123@gmail.com>
2026-06-06 12:20:59 +0400

    feat: add IEmployee

---
+++ FastPay.Server/Services/Interfaces/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿namespace FastPay.Server.Services.Interfaces
+{
+    public interface IEmployee
+    {
+         Guid Id {get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        decimal HourlyRate { get; set; }
+        bool IsActive { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime UpdatedAt { get; set; }
+
+    }
+}
