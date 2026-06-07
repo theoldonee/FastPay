218aeea5a6e63a506e10679f6dfe443c175486e0
theoldonee <rouvaf123@gmail.com>
2026-06-07 18:12:01 +0400

    feat(Services/EmployeeService): add EmployeeService

---
+++ FastPay.Server/Services/EmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using FastPay.Server.Models;
+namespace FastPay.Server.Services
+{
+    // set up db context
+    public class EmployeeService
+    {
+        public async Task StoreEmployee(Employee employee)
+        {
+            // throws error if failure
+        }
+
+        public async Task UpdateEmployee(Employee employee)
+        {
+            // throws error if failure
+        }
+    }
+}
